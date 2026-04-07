837be84cbf6d20cedd773263529e7a977841c103
mazout4ik <42657101+mazout4ik@users.noreply.github.com>
2026-04-07 11:30:54 +0300

    Delete ssl_patch.py

--- ssl_patch.py
+++
@@ -1,6 +0,0 @@
-import ssl
-import certifi
-
-# Force default HTTPS context to use certifi's CA bundle
-ssl._create_default_https_context = ssl.create_default_context
-ssl._create_default_https_context = lambda *args, **kwargs: ssl.create_default_context(cafile=certifi.where())
