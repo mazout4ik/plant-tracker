200c870228dd246fa8442e850830a8506b50bf30
mazout4ik <mazout4ik@gmail.com>
2026-04-07 16:38:26 +0300

    Changes to Overview

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ if page == "Overview":
                     st.write(f"last watered: {last}")
 
                 # This makes the whole card clickable
-                submitted = st.form_submit_button("See details", use_container_width=True)
+                submitted = st.form()
 
             if submitted:
                 st.session_state.selected_id = plant_id
