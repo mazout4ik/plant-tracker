a44838e6546af7eeab4cd0d950b537865e2d27f3
mazout4ik <mazout4ik@gmail.com>
2026-04-14 17:09:21 +0300

    Fix the photo upload

--- app.py
+++ app.py
@@ -212,19 +212,19 @@ elif page == "Add Plant":
                 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
                 ext = uploaded_file.name.split(".")[-1].lower()
                 safe_name = name.lower().replace(" ", "_")
-                photo_path_new = f"{safe_name}_{dt.now().strftime('%Y%m%d_%H%M%S')}.{ext}"
+                photo_path = f"{safe_name}_{dt.now().strftime('%Y%m%d_%H%M%S')}.{ext}"
 
                 try:
-                    file_bytes = new_file.getvalue()
+                    file_bytes = uploaded_file.getvalue()
                     res = supabase.storage.from_(BUCKET).upload(
-                        photo_path_new,          # path
-                        file_bytes,              # file (bytes)
+                        photo_path,
+                        file_bytes,
                         {"upsert": True},        # file_options
                     )
-                    updates["photo_path"] = photo_path_new
+                    st.caption(f"DEBUG upload add: {res}")
                 except Exception as e:
                     st.error(f"Photo upload failed: {e}")
-                    #photo_path = None
+                    photo_path = None
 
             try:
                 data = {
